55b04511a3cd3df4c3bf50d962693a0fd2638478
guluzadehh <quluzade.resul13@mail.ru>
2024-04-09 08:55:34 +0400

    Init tests

---
+++ IpJournal.Tests/IpExtensions.cs
@@ -0,0 +1,103 @@
+﻿using System.Net;
+using IpJournal.Core;
+
+namespace IpJournal.Tests;
+public class IpExtensionsTests
+{
+    [Fact]
+    public void Compare_BothAddressesNull_ReturnsZero()
+    {
+        int result = Extensions.Compare(null, null);
+
+        Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void Compare_FirstAddressNull_ReturnsNegativeOne()
+    {
+        var ipAddress2 = IPAddress.Parse("192.168.1.1");
+
+        int result = Extensions.Compare(null, ipAddress2);
+
+        Assert.Equal(-1, result);
+    }
+
+    [Fact]
+    public void Compare_SecondAddressNull_ReturnsOne()
+    {
+        var ipAddress1 = IPAddress.Parse("192.168.1.1");
+
+        int result = Extensions.Compare(ipAddress1, null);
+
+        Assert.Equal(1, result);
+    }
+
+    [Fact]
+    public void Compare_SameAddresses_ReturnsZero()
+    {
+        var ipAddress1 = IPAddress.Parse("192.168.1.1");
+        var ipAddress2 = IPAddress.Parse("192.168.1.1");
+
+        int result = ipAddress1.Compare(ipAddress2);
+
+        Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void Compare_FirstAddressBeforeSecond_ReturnsNegativeOne()
+    {
+        var ipAddress1 = IPAddress.Parse("192.168.1.1");
+        var ipAddress2 = IPAddress.Parse("192.168.1.2");
+
+        int result = ipAddress1.Compare(ipAddress2);
+
+        Assert.Equal(-1, result);
+    }
+
+    [Fact]
+    public void Compare_FirstAddressAfterSecond_ReturnsOne()
+    {
+        var ipAddress1 = IPAddress.Parse("192.168.1.2");
+        var ipAddress2 = IPAddress.Parse("192.168.1.1");
+
+        int result = ipAddress1.Compare(ipAddress2);
+
+        Assert.Equal(1, result);
+    }
+
+    [Fact]
+    public void IsInSubnet_IPAddressInSubnet_ReturnsTrue()
+    {
+        var ipAddress = IPAddress.Parse("192.168.1.1");
+        var subnetAddress = IPAddress.Parse("192.168.1.0");
+        int subnetMaskLength = 24;
+
+        bool result = ipAddress.IsInSubnet(subnetAddress, subnetMaskLength);
+
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void IsInSubnet_IPAddressNotInSubnet_ReturnsFalse()
+    {
+        var ipAddress = IPAddress.Parse("192.168.1.1");
+        var subnetAddress = IPAddress.Parse("10.0.0.0");
+        int subnetMaskLength = 8;
+
+        bool result = ipAddress.IsInSubnet(subnetAddress, subnetMaskLength);
+
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void IsInSubnet_IPAddressInSubnetWithMaskLessThan8_ReturnsTrue()
+    {
+        var ipAddress = IPAddress.Parse("192.168.1.1");
+        var subnetAddress = IPAddress.Parse("192.168.1.0");
+        int subnetMaskLength = 24;
+
+        bool result = ipAddress.IsInSubnet(subnetAddress, subnetMaskLength);
+
+        Assert.True(result);
+    }
+}
---
+++ IpJournal.Tests/RequestLogsFilterTest.cs
@@ -0,0 +1,122 @@
+﻿using System.Net;
+using IpJournal.Core.Filter;
+using IpJournal.Core.Models;
+
+namespace IpJournal.Tests
+{
+    public class RequestLogsFilterTests
+    {
+        [Fact]
+        public void Filter_NoFilters_ReturnsAllRequestLogs()
+        {
+            var filter = new RequestLogsFilter();
+            var requestLogs = new List<RequestLog>
+            {
+                new RequestLog(IPAddress.Parse("192.168.1.1"), DateTimeOffset.Now),
+                new RequestLog(IPAddress.Parse("192.168.1.2"), DateTimeOffset.Now),
+                new RequestLog(IPAddress.Parse("192.168.1.3"), DateTimeOffset.Now)
+            };
+
+            var filteredLogs = filter.Filter(requestLogs, new FilterOptions(DateTimeOffset.MinValue, DateTimeOffset.MaxValue));
+
+            Assert.Equal(requestLogs.Count, filteredLogs.Count());
+        }
+
+        [Fact]
+        public void Filter_WithIpAddressFilter_ReturnsFilteredRequestLogs()
+        {
+            var ipAddressToFilter = IPAddress.Parse("192.168.1.1");
+            var filter = new RequestLogsFilter((log, options) => log.IpAddress.Equals(ipAddressToFilter));
+            var requestLogs = new List<RequestLog>
+            {
+                new RequestLog(IPAddress.Parse("192.168.1.1"), DateTimeOffset.Now),
+                new RequestLog(IPAddress.Parse("192.168.1.2"), DateTimeOffset.Now),
+                new RequestLog(IPAddress.Parse("192.168.1.3"), DateTimeOffset.Now)
+            };
+
+            var filteredLogs = filter.Filter(requestLogs, new FilterOptions(DateTimeOffset.MinValue, DateTimeOffset.MaxValue));
+
+            Assert.Single(filteredLogs);
+            Assert.Equal(ipAddressToFilter, filteredLogs.First().IpAddress);
+        }
+
+        [Fact]
+        public void Filter_WithDateRangeFilter_ReturnsFilteredRequestLogs()
+        {
+            var startDate = DateTimeOffset.Parse("2024-01-01");
+            var endDate = DateTimeOffset.Parse("2024-01-10");
+            var filter = new RequestLogsFilter((log, options) => log.Date >= options.StartDate && log.Date <= options.EndDate);
+            var requestLogs = new List<RequestLog>
+            {
+                new RequestLog(IPAddress.Parse("192.168.1.1"), DateTimeOffset.Parse("2024-01-05")),
+                new RequestLog(IPAddress.Parse("192.168.1.2"), DateTimeOffset.Parse("2024-01-09")),
+                new RequestLog(IPAddress.Parse("192.168.1.3"), DateTimeOffset.Parse("2024-01-15"))
+            };
+
+            var filteredLogs = filter.Filter(requestLogs, new FilterOptions(startDate, endDate));
+
+            Assert.Equal(2, filteredLogs.Count());
+            Assert.All(filteredLogs, log => Assert.True(log.Date >= startDate && log.Date <= endDate));
+        }
+
+        [Fact]
+        public void Register_SingleCallback_AddsCallbackToList()
+        {
+            var filter = new RequestLogsFilter();
+            FilterCb callback = (log, options) => true;
+
+            filter.Register(callback);
+
+            Assert.Contains(callback, filter.FilterCbs);
+        }
+
+        [Fact]
+        public void Register_MultipleCallbacks_AddsAllCallbacksToList()
+        {
+            var filter = new RequestLogsFilter();
+            var callbacks = new List<FilterCb>
+            {
+                (log, options) => true,
+                (log, options) => false
+            };
+
+            filter.Register(callbacks);
+
+            Assert.Equal(callbacks.Count, filter.FilterCbs.Count());
+            Assert.All(callbacks, callback => Assert.Contains(callback, filter.FilterCbs));
+        }
+
+        [Fact]
+        public void Filter_WithNoMatchingFilters_ReturnsEmptyList()
+        {
+            var filter = new RequestLogsFilter((log, options) => log.IpAddress.Equals(IPAddress.Parse("192.168.1.2")) && log.Date > DateTimeOffset.Parse("2024-01-01"));
+            var requestLogs = new List<RequestLog>
+            {
+                new RequestLog(IPAddress.Parse("192.168.1.1"), DateTimeOffset.Parse("2024-01-05")),
+                new RequestLog(IPAddress.Parse("192.168.1.1"), DateTimeOffset.Parse("2024-01-09")),
+                new RequestLog(IPAddress.Parse("192.168.1.1"), DateTimeOffset.Parse("2024-01-15"))
+            };
+
+            var filteredLogs = filter.Filter(requestLogs, new FilterOptions(DateTimeOffset.MinValue, DateTimeOffset.MaxValue));
+
+            Assert.Empty(filteredLogs);
+        }
+
+        [Fact]
+        public void Filter_NullRequestLogs_ThrowsArgumentNullException()
+        {
+            var filter = new RequestLogsFilter();
+
+            Assert.Throws<ArgumentNullException>(() => filter.Filter(null, new FilterOptions(DateTimeOffset.MinValue, DateTimeOffset.MaxValue)));
+        }
+
+        [Fact]
+        public void Filter_NullFilterOptions_ThrowsArgumentNullException()
+        {
+            var filter = new RequestLogsFilter();
+            var requestLogs = new List<RequestLog>();
+
+            Assert.Throws<ArgumentNullException>(() => filter.Filter(requestLogs, null));
+        }
+    }
+}
